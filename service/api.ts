38ca66282e09ab905ae3a56880f10e1cd2611b23
OctavioTchamba <octaviotchamba9@gmail.com>
2026-02-12 14:34:35 +0100

    change api

--- service/api.ts
+++ service/api.ts
@@ -1,41 +1,21 @@
 import axios from 'axios';
 
 const getBaseURL = () => {
-  // Se estivermos no servidor (SSR), usamos localhost ou variável de ambiente
-  if (typeof window === 'undefined') {
+  // Produção
+  if (process.env.NODE_ENV === 'production') {
     return process.env.NEXT_PUBLIC_API_URL || 'https://cebea-server-production.up.railway.app/api';
   }
-
-  const hostname = 'https://cebea-server-production.up.railway.app/api';
   
-  // Se for produção (domínio real), usa a variável de ambiente
-  if (process.env.NODE_ENV === 'production') {
-    return process.env.NEXT_PUBLIC_API_URL;
-  }
-
-  // Se for desenvolvimento, detecta o IP dinamicamente para testes em telemóvel
-  return `https://${hostname}`;
+  // Desenvolvimento
+  return 'http://localhost:8080/api';  // ou a porta que seu servidor usa
 };
 
 const api = axios.create({
   baseURL: getBaseURL(),
-  withCredentials: true, // Essencial para cookies HttpOnly
-});
-
-export default api;
-
-
-
-/*
-// src/services/api.ts
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:8000/api/user', 
-  withCredentials: true, // Essencial para enviar/receber Cookies HttpOnly
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-export default api;*/
+export default api;
